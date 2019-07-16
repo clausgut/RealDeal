48aef10ac27f52ddb5999bc46d6991fb98d9d50d
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-07-16 04:48:28 -0400

    navbar

---
+++ realdeal/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+function Navbar(){
+return(
+<nav className="navbar navbar-light bg-light">
+  <a className="navbar-brand" href="#">
+    <img src="img" width="30" height="30" class="d-inline-block align-top" alt="">
+    RealDeal
+  </a>
+</nav>
+);
+
+}
