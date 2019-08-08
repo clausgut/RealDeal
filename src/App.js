65106d275548fcf26e9b589fb6111cdc8171cb4b
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-08-08 02:49:11 -0400

    src folder

--- src/App.js
+++ src/App.js
@@ -18,22 +18,134 @@ import Comments from './components/comments'
 import Footer from './components/footer'
 
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+}
+this.getUser = this.getUser.bind(this)
+this.componentDidMount = this.componentDidMount.bind(this)
+this.updateUser = this.updateUser.bind(this)
+}
+componentDidMount() {
+  this.getUser()
+}
+
+updateUser(userObject) {
+  this.setState(userObject)
+}
+
+getUser() {
+  axios.get('/user/').then(response => {
+    console.log('Get user response: ')
+    console.log(response.data)
+    if (response.data.user) {
+      console.log('Get User: There is a user saved in the server session: ')
+
+      this.setState({
+        loggedIn: true,
+        username: response.data.user.username
+      })
+    } else {
+      console.log('Get user: no user');
+      this.setState({
+        loggedIn: false,
+        username: null
+      })
+    }
+  })
+}
+
+render() {
   return (
-  
-      <div>
-        <Hero />
-        <Navbar />
-        {/* <SearchResultContainer />
-        <SearchForm /> */}
-        <itemCard
-        // addWishList={this.addWishList}
-        // id={item.id}
-        // name={item.name}
-        />
-        <div style={{ height: "1000px"}}>simulating a lot of content</div>
-      
-        </div>
-  )};
+    <div className="App">
+
+      <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+      <Route
+        exact path="/"
+        component={Home} />
+      <Route
+        path="/login"
+        render={() =>
+          <LoginForm
+            updateUser={this.updateUser}
+          />}
+      />
+      <Route
+        path="/signup"
+        render={() =>
+          <Signup
+            signup={this.signup}
+          />}
+      />
+      <Route
+        exact path="/forum"
+        render={() =>
+          <Forum
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/newstory"
+        render={() =>
+          <NewStory
+            username={this.state.username}
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/forum/funny"
+        render={() =>
+          <Funny
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/forum/horror"
+        render={() =>
+          <Horror
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/forum/mystery"
+        render={() =>
+          <Mystery
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/forum/fantasy"
+        render={() =>
+          <Fantasy
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/forum/romance"
+        render={() =>
+          <Romance
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        path="/forum/drama"
+        render={() =>
+          <Drama
+            loggedIn={this.state.loggedIn}
+          />}
+      />
+      <Route
+        exact path="/story/:id"
+        component={Comments} />
+      <Footer />
+    </div>
+  );
+}
+}
+
 export default App;
+
         
