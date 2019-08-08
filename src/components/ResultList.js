1b73d28196d3952f6a955174d7ce85d4aceb9fa4
Claudia Gutierrez <gutierrez.cp21@yahoo.com>
2019-08-08 03:17:13 -0400

    resultlist update

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-function ResultList(props){
-    return(
-        <ul className="list-group">
-            {props.results.map(result => (
-                <li className="list-group-item" key={result.id}>
-                    <img alt={result.title} className="img-fluid" src={result.images.original.url} />
-
-                </li>
-            ))}
-        </ul>
+function ResultList(props) {
+    return (
+        <li className="list-group">
+            <div className="card-header">{props.title}</div>
+            <div className="card-body">
+                <p className="card-text">{props.description}</p>
+                <p className="card-text">Author: <strong>{props.author}</strong></p>
+                <Link to={"/story/" + props.id} className="btn btn-primary" role="button">Add to this story</Link>
+            </div>
+        </li>
     );
 }
 
